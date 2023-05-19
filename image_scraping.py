5577dc24c6a66ef8370f13b75eb1022b59fda6b4
neilchristopher2002 <106363571+neilchristopher2002@users.noreply.github.com>
2023-05-19 10:50:09 +0700

    add cleaning function
    add cleaning function and dataset for spice cinnamon

--- image_scraping.py
+++ image_scraping.py
@@ -2,6 +2,10 @@ import requests
 import urllib.request
 import re
 from bs4 import BeautifulSoup
+import socket
+
+# Set timeout 30 detik biar ga error
+socket.setdefaulttimeout(30)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36"
@@ -33,6 +37,7 @@ def search_data(soup):
             "chips": "".join(re.findall(r"&chips=(.*?)&", suggested_search.a["href"])),
             "thumbnail": bytes(suggested_search_fixed_thumbnail, "ascii").decode("unicode-escape")
         })
+
     print('start')
     return suggested_searches
 
@@ -41,11 +46,12 @@ def get_image(headers, parameter):
     google_images = []
     num_images = 100
     start_index = 0
-    path = 'C:\\Users\\Christopher\\Downloads\\Capstone\\Dataset\\Test'
+    path = 'D:\Kuliah\Bangkit\Capstone\CapstoneProject\Dataset\Cinnamon_5'
+    image_index = 1 # Set index gambar biar berurutan
 
     while len(google_images) < num_images:
         parameter['start'] = start_index
-        html = requests.get("https://www.google.com/search", params=parameter, headers=headers, timeout=30)
+        html = requests.get("https://www.google.com/search", params=parameter, headers=headers, timeout=30) # Tambah timeout
         soup = BeautifulSoup(html.text, "lxml")
         
         all_script_tags = soup.select('script')
@@ -70,8 +76,8 @@ def get_image(headers, parameter):
             matched_google_full_resolution_images
         ]
 
-        for index, (metadata, thumbnail, original) in enumerate(
-                zip(soup.select('.isv-r.PNCib.MSM1fd.BUooTd'), thumbnails, full_res_images), start=start_index + 1):
+        for metadata, thumbnail, original in zip(
+                soup.select('.isv-r.PNCib.MSM1fd.BUooTd'), thumbnails, full_res_images):
             google_images.append({
                 "title": metadata.select_one(".VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb")["title"],
                 "link": metadata.select_one(".VFACy.kGQAp.sMi44c.lNHeqe.WGvvNb")["href"],
@@ -79,18 +85,19 @@ def get_image(headers, parameter):
                 "thumbnail": thumbnail,
                 "original": original
             })
-            print(f'Downloading {index} image...')
 
-            opener = urllib.request.build_opener()
-            opener.addheaders = [('User-Agent',
-                                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36')]
-            urllib.request.install_opener(opener)
+            print(f'Downloading {image_index} image...')
 
-            try: 
-                urllib.request.urlretrieve(original, f'{path}\\cinnamon_{index}.jpg')
-            except:
-                print('error')
-            
+            try:
+                # Implementasi timeout
+                response = requests.get(original, timeout=30) # Tambah timeout
+                with open(f'{path}\\cinnamon_{image_index}.jpg', 'wb') as file:
+                    file.write(response.content)
+            except requests.exceptions.RequestException as e:
+                print('Error:', e)
+
+            image_index += 1
+
             if len(google_images) == num_images:
                 break
 
@@ -99,9 +106,39 @@ def get_image(headers, parameter):
     print('end')
     return google_images
 
+# Cleaning
+def clean_data(data):
+    cleaned_data = []
+    unique_titles = set()
 
-html = requests.get("https://www.google.com/search", params=parameter, headers=headers, timeout=30)
+    for item in data:
+        cleaned_item = {
+            'title': item['title'],
+            'link': item['link'],
+            'source': item['source'],
+            'thumbnail': item['thumbnail'],
+            'original': item['original']
+        }
+
+        if cleaned_item['title'] not in unique_titles:
+            cleaned_data.append(cleaned_item)
+            unique_titles.add(cleaned_item['title'])
+
+    for item in cleaned_data:
+        print("Title:", item['title'])
+        print("Link:", item['link'])
+        print("Source:", item['source'])
+        print("Thumbnail:", item['thumbnail'])
+        print("Original:", item['original'])
+        print()
+
+    return cleaned_data
+
+
+html = requests.get("https://www.google.com/search", params=parameter, headers=headers, timeout=30) # Tambah timeout
 soup = BeautifulSoup(html.text, "lxml")
 
 search_data(soup)
-get_image(headers, parameter)
+
+raw_data = get_image(headers, parameter)
+cleaned_data = clean_data(raw_data)
